43ec9a14e88e91f9092927613f1c4aa5886535e8
NightFury3000 <shubhsaxena447@gmail.com>
2021-03-23 16:43:07 +0530

    added run animation and controls are more responsive now

--- Dimensions/Assets/PlayerMovement.cs
+++ Dimensions/Assets/PlayerMovement.cs
@@ -2,12 +2,23 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator playerAnim;
+    private enum State { idle,running}
+    private State state = State.idle;
+
+
     [SerializeField] private float speed = 3f;
     // Update is called once per frame
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+    }
     private void Update()
     {
         move();
+        playerState();
     }
 
     private void move()
@@ -23,6 +34,19 @@ public class PlayerMovement : MonoBehaviour
          {
              rb.velocity = new Vector2(-speed, rb.velocity.y);
              transform.localScale = new Vector2(-0.14332f, transform.localScale.y);
+        }
+        playerAnim.SetInteger("State", (int)state);
+    }
+
+    public void playerState()
+    {
+        if(Mathf.Abs(rb.velocity.x) > 1.5f)
+        {
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
         }
     }
      
