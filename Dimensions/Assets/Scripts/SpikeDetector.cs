75c2f464336ea03400a0b132bebf4eea6409cf24
NightFury3000 <shubhsaxena447@gmail.com>
2021-03-26 15:58:36 +0530

    Finished Level 2(Mechanics Wise) and the added a better spike trigger system
    I redid the mechanics to make it more reusable for the further levels

---
+++ Dimensions/Assets/Scripts/SpikeDetector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SpikeDetector : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 spawnPoint;
+
+    private Rigidbody2D rb;
+    [SerializeField]
+    private LayerMask spike;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(rb.IsTouchingLayers(spike))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
