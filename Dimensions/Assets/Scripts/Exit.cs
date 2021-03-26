1be75826478e888a4e49297fdafc5ec52cd08ebc
NightFury3000 <shubhsaxena447@gmail.com>
2021-03-26 18:47:28 +0530

    Added a basic main menu.(It needs work on help and options section)
    #Announcements in discord has my ideal
    If you can please apply it

---
+++ Dimensions/Assets/Scripts/Exit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("HI0;");
+            Application.Quit();
+        }
+    }
+}
---
+++ Dimensions/Assets/Scripts/Play.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
