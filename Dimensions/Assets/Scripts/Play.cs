36b43b6e1e39ce0b3b23f14d61e36cfa8c5ec4cb
Chrid_The_Scholar <ChridCoProfessional@gmail.com>
2021-03-26 21:29:57 -0400

    Added Music on play. Scene Changer is revamped

---
+++ Dimensions/Assets/Scripts/Play.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+			FindObjectOfType<AudioManager>().Play("Music");
+        }
+    }
+}
